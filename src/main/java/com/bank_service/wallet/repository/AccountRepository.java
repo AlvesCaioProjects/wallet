a41d3740b3318cf5f0f059b1011369154b46a88d
Caio Alves <caioanegocios@gmail.com>
2026-03-05 11:16:11 -0300

    Create repository

---
+++ src/main/java/com/bank_service/wallet/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.bank_service.wallet.repository;
+
+import com.bank_service.wallet.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+}
